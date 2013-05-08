ac18bb5a4999dd7e3adc49ddcbde8c14c51ca5fc
André <andre@linux.fritz.box>
2013-05-08 18:08:09 +0200

    initial commit

---
+++ src/mail2printgw/Mail2printGW.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mail2printgw;
+
+/**
+ *
+ * @author andre
+ */
+public class Mail2printGW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
